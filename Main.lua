2ea9afcfd03cbc91a14c1141eec45d1ba3353e7d
LLEVC <71854739+LLEVC@users.noreply.github.com>
2025-09-28 00:29:22 -0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -7,3 +7,11 @@ local button = toolbar:CreateButton("Button", "This will soon be changed...", "r
 button.Click:Connect(function()
 	print("visit https://github.com/jswagging/Published-Community-Made-Roblox-Plugin to make changes!")
 end)
+
+local button = toolbar:CreateButton("Button That Destroys Everything in The Workspace", "Button That Destroys Everything in The Workspace", "rbxassetid://74783578358968")
+
+button.Click:Connect(function()
+	for i,v in workspace:GetChildren() do
+		game:GetService("Debris"):AddItem(v,math.random(1,1000)/100)
+	end
+end)
