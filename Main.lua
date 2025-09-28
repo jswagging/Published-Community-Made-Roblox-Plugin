231318e784979cc7c62fc6e10cf8e2a123b656d2
Cametized <cameron.reese.f@gmail.com>
2025-09-28 11:12:19 -0400

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -3,11 +3,28 @@
 
 local toolbar = plugin:CreateToolbar("PCMRP")
 local message = "visit https://github.com/jswagging/Published-Community-Made-Roblox-Plugin to make changes!"
-local button = toolbar:CreateButton("Button That Destroys Everything in The Workspace", "Button That Destroys Everything in The Workspace", "rbxassetid://74783578358968")
+local destroy = toolbar:CreateButton("Button That Destroys Everything in The Workspace", "Button That Destroys Everything in The Workspace", "rbxassetid://74783578358968")
+local randomlighting = toolbar:CreateButton("Randomize Game Lighting", "self explanatory", "rbxassetid://108545913588312")
 
-button.Click:Connect(function()
+local function randomlight()
+	game.Lighting.Brightness = math.random(1, 10)
+	game.Lighting.ClockTime = math.random() * 24
+	game.Lighting.Ambient = Color3.new(math.random(), math.random(), math.random())
+	game.Lighting.OutdoorAmbient = Color3.new(math.random(), math.random(), math.random())
+	game.Lighting.FogColor = Color3.new(math.random(), math.random(), math.random())
+	game.Lighting.FogStart = math.random(0, 100)
+	game.Lighting.FogEnd = math.random(200, 1000)
+	game.Lighting.EnvironmentDiffuseScale = math.random()
+	game.Lighting.EnvironmentSpecularScale = math.random()
+	game.Lighting.ExposureCompensation = math.random(-2, 2)
+end
+
+destroy.Click:Connect(function()
 	print(message)
 	for i,v in workspace:GetChildren() do
 		game:GetService("Debris"):AddItem(v,math.random(1,1000)/100)
 	end
 end)
+
+randomlighting.Click:Connect(randomlight)
+
