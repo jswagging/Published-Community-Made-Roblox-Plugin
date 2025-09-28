d5e2d4ce72a1d522aedd1ee91465d38e13c9ca24
jswagging <jamerchgitcontactmail@yahoo.com>
2025-09-28 05:37:15 -0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,9 +1,13 @@
--- Add anything... (by forking this project)
--- Every change will have to be approved by JamerchGIT!
+-- Add anything...
+-- Every change will be approved by JamerchGIT!
 
 local toolbar = plugin:CreateToolbar("PCMRP")
-local button = toolbar:CreateButton("Button", "This will soon be changed...", "rbxassetid://88107201574608")
+local message = "visit https://github.com/jswagging/Published-Community-Made-Roblox-Plugin to make changes!"
+local button = toolbar:CreateButton("Button That Destroys Everything in The Workspace", "Button That Destroys Everything in The Workspace", "rbxassetid://74783578358968")
 
 button.Click:Connect(function()
-	print("visit https://github.com/jswagging/Published-Community-Made-Roblox-Plugin to make changes!")
+	print(message)
+	for i,v in workspace:GetChildren() do
+		game:GetService("Debris"):AddItem(v,math.random(1,1000)/100)
+	end
 end)
