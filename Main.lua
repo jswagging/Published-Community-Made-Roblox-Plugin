d46fae0d6b047c8a8d2f14640cb5fd2ad838503f
jswagging <jamerchgitcontactmail@yahoo.com>
2025-09-27 22:55:47 -0700

    Create Main.lua

---
+++ Main.lua
@@ -0,0 +1,9 @@
+-- Add anything... (by forking this project)
+-- Every change will have to be approved by JamerchGIT!
+
+local toolbar = plugin:CreateToolbar("PCMRP")
+local button = toolbar:CreateButton("Button", "This will soon be changed...", "rbxassetid://88107201574608")
+
+button.Click:Connect(function()
+	print("visit https://github.com/jswagging/Published-Community-Made-Roblox-Plugin to make changes!")
+end)
